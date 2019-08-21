7a1a12ab846ce6b92ea22d74731749734a21843d
tyler <tyler.william.palmer@gmail.com>
2019-08-21 14:41:28 -0600

    setup main endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+//Imports
 require('dotenv').config()
 const express = require('express')
 const session = require('express-session')
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 9000
 //Controllers
 const adminController = require('./controllers/admin_controller')
 const cloudinaryController = require('./controllers/cloudinary_controller')
-const productsController = require('./constrollers/products_controller')
+const productsController = require('./controllers/products_controller')
 const userController = require('./controllers/user_controller')
 
 //Middleware
@@ -29,6 +30,24 @@ app.use(session({
 app.use(cors())
 
 //Routes
+setTimeout(() => {
+    //User Endpoints
+    app.get('/api/user-data', userController.readUserData)
+    app.post('/api/user-data/cart', userController.addToCart)
+    app.delete('/api/user-data/cart/:id',userController.removeFromCart)
+    app.post('/api/login', userController.login)
+    app.post('/api/logout', userController.logout)
+
+    //Product Endpoints
+    app.get('/api/products', productsController.readAllProducts)
+    app.get('/api/products/:id', productsController.readProduct)
+
+    //Admin Endpoints
+    app.get('/api/users', adminController.getAdminUsers)
+    app.post('/api/products', adminController.createProduct)
+    app.put('/api/products/:id', adminController.updateProduct)
+    app.delete('/api/products/:id', adminController.deleteProduct)
+    }, 200)
 
 //Database
 mongoose.connect(process.env.CONNECTION_STRING || 'mongodb://localhost:27017/mern-ecommerce', {useNewUrlParser : true}, () => {
