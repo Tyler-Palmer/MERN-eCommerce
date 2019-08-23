288c2d0b45783db18d2d8ddc82b7b33074b74f3b
tyler <tyler.william.palmer@gmail.com>
2019-08-22 23:44:36 -0600

    created stateful About and Cart components

---
+++ client/src/components/container/Cart/Cart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Cart.css'
+
+class Cart extends Component{
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default Cart
